1ae5c037db4e996eae01041756228ba14c0eb682
203lir <44447785+203lir@users.noreply.github.com>
2020-10-19 20:55:49 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JerBoris
  Discord JerBoris bot
+To add JerBoris to your server, use this link: https://discordapp.com/oauth2/authorize?client_id=758107051401805842&scope=bot
---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('Logged in as ${client.user.tag}!');
+});
+
+client.on('message', message => {
+	if (message.author === client.user) {
+		return
+	}
+	if (message.content === ';;jerboa') {
+		message.channel.send({files: ['https://www.zsl.org/sites/default/files/image/2014-02/Long-eared-jerboa.jpg']});
+	}
+	if ((message.content.search(/shriven/i)) != -1) {
+		message.react('766315936667271168');
+	}
+	if ((message.content.search(/trajan/i)) != -1) {
+		message.react('637432815377448971');
+	}
+	if ((message.content.search(/rome/i)) != -1) {
+		message.react('637435059087081513');
+	}
+});
+
+client.login('removedforsecurity');
